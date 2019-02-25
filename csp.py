5f98935e9c671cd7b7c47ae52cafa878661bb33a
Kevin Klinkel <kgklinkel@gmail.com>
2019-02-25 09:33:43 +0100

    now it places all the heliostats!

--- csp.py
+++ csp.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
 import numpy as np
+import matplotlib.pyplot as plt
+
+# make some references to np functions, for ease of coding
 pi  = np.pi
 cos = np.cos
 sin = np.sin
@@ -8,6 +11,11 @@ tan = np.tan
 arccos = np.arccos
 arcsin = np.arcsin
 arctan = np.arctan
+
+# make a global variable for holding the net power
+net_power_thermal = 0.0
+# global list for all heliostats in solar field
+solar_field = []
 
 print("============================================")
 
@@ -45,6 +53,7 @@ GEOMETRY
     - curvature of Earth is neglected in terms of altitude (z-axis) change
         --> only about 0.2m difference
             40000*(1-sin(arccos(4/40000))) = 0.00019km (0.19m)
+    - maintenance roads are not accounted for
 
 HELIOSTATS
     - mirror area / total area = 0.96
@@ -61,6 +70,8 @@ RECEIVER
           all around, if you neglect vertical changes
     - receiver length is assumed long enough to avoid vertical spillage
 
+POWER
+    - (parasitic?) power used (ex: pumps) is not considered
 """
 #===============================================================================
 # GIVEN PARAMETERS
@@ -72,6 +83,10 @@ NET_POWER_W             = NET_POWER_KW * 1000 #[W]
 EFF_PB                  = 0.43
 EFF_RECEIVER_THERMAL    = 0.88
 EFF_GENERATOR           = 0.98
+
+NET_POWER_TH_W          = NET_POWER_W / (EFF_PB
+                                         * EFF_RECEIVER_THERMAL
+                                         * EFF_GENERATOR)
                         
 MIRROR_AREA             = 115 #[m2]
 MIRROR_AREA_TO_TOTAL    = 0.96 # (assumption)
@@ -128,6 +143,7 @@ ALTITUDE  = LAT_LON_CENTER[2]   # (z)
 
 MERIDIAN  = -3*15 #[deg E]
 
+
 #-------------------------------------------------------------------------------
 # translate lat/long points into (x,y) coordinates [m]
 def lat_lon_to_dx_dy(p1, p2):
@@ -135,16 +151,40 @@ def lat_lon_to_dx_dy(p1, p2):
     (lon2, lat2, alt2) = p2
     dx = 1000 * (lon2-lon1)*40000*cos((lat1+lat2)*pi/360)/360
     dy = 1000 * (lat2-lat1)*40000/360
-    return (dx, dy)
+    dz = 1000 * (alt2-alt1)
+    return (dx, dy, dz)
 
 print("Perimeter (x,y) coordinates:")
-xy_points = []
+perimeter_xyz_points = []
 for p in LAT_LON_POINTS:
-    (dx,dy) = lat_lon_to_dx_dy(LAT_LON_CENTER, p)
-    xy_points.append((dx,dy))
+    (dx,dy,dz) = lat_lon_to_dx_dy(LAT_LON_CENTER, p)
+    perimeter_xyz_points.append((dx,dy,dz))
     print("    ({: 6.0f}, {: 6.0f} ) [m]".format(dx,dy))
-xy1, xy2, xy3, xy4, xy5 = xy_points
+xyz1, xyz2, xyz3, xyz4, xyz5 = perimeter_xyz_points
 print()
+
+
+#-------------------------------- PLOT POINTS ----------------------------------
+# create plot (turn on interactive mode)
+plt.ion()
+
+# plot perimeter points
+pp = list(perimeter_xyz_points)
+pp.append(perimeter_xyz_points[0]) # append [0] element, so it draws full perimeter
+ppx, ppy, ppz = zip(*pp)
+plot = plt.scatter(ppx, ppy, color='k', marker='')
+
+# draw perimeter border
+plt.plot(ppx, ppy, 'k-', color='k', linewidth=1)
+
+# plot center point (tower position)
+ppx, ppy, ppz = [0], [0], [0]
+plt.scatter(ppx, ppy, color='r', marker='o', s=50)
+# plt.scatter([1000], [1000], color='g')
+
+# show plot
+plt.show(block=False)
+
 
 
 #------------------------------ Solar parameters -------------------------------
@@ -176,7 +216,7 @@ N = (24/pi)*arccos( -1*tan(lat) * tan(declination) )
 # v_S = np.array(( sin(zenith)*sin(azimuth),
 #                  sin(zenith)*cos(azimuth),
 #                  cos(zenith) ))
-v_S = np.array( (0,sin(zenith),cos(zenith)) )
+v_S = np.array((0, sin(zenith), cos(zenith)))
 
 
 print("Solar parameters:")
@@ -192,7 +232,6 @@ print()
 
 
 #-------------------------------------------------------------------------------
-# generate list of heliostats (coordinates)
 class Heliostat(object):
     """
     |-------| Tower height                                      
@@ -226,22 +265,31 @@ class Heliostat(object):
         param: position [np.array] - (x, y, z=0) coordinate from tower base
         """
         # vector from tower base
-        self.position = position
-        # initiate vector attributes
-        self.vT  = np.ones(3)
-        self.nH  = np.ones(3)
-        self.v_T = np.ones(3)
-        self.n_H = np.ones(3)
-        self.dR  = 0.0
+        self.position = position    # (x,y,z) coordinate, with tower at (0,0,0)
 
-        # self.slope_angle        = 0.0
-        # self.azimuth_angle      = 0.0
-        # self.theta              = 0.0
+        # initiate vector attributes
+        self.vT  = np.ones(3)   # vector to tower (receiver)
+        self.nH  = np.ones(3)   # normal vector of mirror surface
+        self.v_T = np.ones(3)   # UNIT vector to tower (receiver)
+        self.n_H = np.ones(3)   # UNIT normal vector of mirror surface
+        self.dR  = 0.0          # distance to receiver [m]
+
+        # angles
+        self.slope_angle        = 0.0
+        self.azimuth_angle      = 0.0
+        self.theta              = 0.0
+
+        # loss factors
         self.cosine_eff         = 1.0
         self.f_shadow_block     = 0.0
         self.f_att              = 0.0
         self.f_spill            = 0.0
-        self.total_contribution = 0.0   # (total kW_th to receiver)
+
+        # total energy contributed to the receiver
+        self.total_contribution = 0.0   # (total W_th to receiver)
+
+        # calculate total contribution
+        self.calculate_energy_contribution()
 
     def calculate_n_H(self):
         """calculate the surface normal vector"""
@@ -262,6 +310,7 @@ class Heliostat(object):
     def determine_cosine_effectiveness(self):
         # (same as theta)
         self.cosine_eff = np.dot(self.v_T, self.n_H)
+        self.theta = self.cosine_eff
 
     def determine_shadow_block(self):
         # (assumed to be ~5%, for simplicity)
@@ -292,31 +341,34 @@ class Heliostat(object):
         else:
             self.f_spill = 0.0
 
-    def calculate_energy_contribution(self):
+    def calculate_energy_contribution(self, verbose=False):
         # determine the heliostat's relevant parameters
-        print(".................................................")
-        print("calculating heliostat parameters at position:")
-        print("  ({},{},{}) [m]".format(*self.position), end="")
+        if verbose:
+            print(".................................................")
+            print("calculating heliostat parameters at position:")
+            print("  ({},{},{}) [m]".format(*self.position), end="")
         self.set_angle()
-        print("  --> dR = {:,.1f} [m]".format(self.dR))
-        print(".................................................")
+        if verbose:
+            print("  --> dR = {:,.1f} [m]".format(self.dR))
+            print(".................................................")
         self.determine_cosine_effectiveness()
         self.determine_shadow_block()
         self.determine_attenuation()
         self.determine_spillage()
 
         #-----------------------------------------------------------------------
-        print("=============================")
-        print("NOM_Ib: {}  [W]".format(NOM_Ib))
-        print("area:   {:04.1f} [m2]".format(Heliostat.area))
-        print("    _________________________")
-        print("    reflectivity:    {: 04.3f}".format(Heliostat.reflectivity))
-        print("    clean_factor:    {: 04.3f}".format(Heliostat.clean_factor))
-        print("    cosine_eff:      {: 04.3f}".format(self.cosine_eff))
-        print("    f_shadow_block:  {: 04.3f}".format(self.f_shadow_block))
-        print("    f_att:           {: 04.3f}".format(self.f_att))
-        print("    f_spill:         {: 04.3f}".format(self.f_spill))
-        print("    _________________________")
+        if verbose:
+            print("=============================")
+            print("NOM_Ib: {}  [W]".format(NOM_Ib))
+            print("area:   {:04.1f} [m2]".format(Heliostat.area))
+            print("    _________________________")
+            print("    reflectivity:    {: 04.3f}".format(Heliostat.reflectivity))
+            print("    clean_factor:    {: 04.3f}".format(Heliostat.clean_factor))
+            print("    cosine_eff:      {: 04.3f}".format(self.cosine_eff))
+            print("    f_shadow_block:  {: 04.3f}".format(self.f_shadow_block))
+            print("    f_att:           {: 04.3f}".format(self.f_att))
+            print("    f_spill:         {: 04.3f}".format(self.f_spill))
+            print("    _________________________")
         total_efficiency = (
               Heliostat.reflectivity
             * Heliostat.clean_factor
@@ -325,39 +377,128 @@ class Heliostat(object):
             * (1.0 - self.f_att)
             * (1.0 - self.f_spill)
         )
-        print("    TOTAL EFFICIENCY: {:04.1f} %".format(total_efficiency*100))
-        print()
+        if verbose:
+            print("    TOTAL EFFICIENCY: {:04.1f} %".format(total_efficiency*100))
+            print()
         
         # calculate total solar thermal energy delivered to the receiver [kW_th]
         self.total_contribution = NOM_Ib * Heliostat.area * total_efficiency
 
         # print result
-        print("--> total_contribution: {:,.0f} [W]".format(
-            self.total_contribution))
-        print("                        {:.1f}  [kW]".format(
-            self.total_contribution/1000))
-        print()
+        if verbose:
+            print("--> total_contribution: {:,.0f} [W]".format(
+                self.total_contribution))
+            print("                        {:.1f}  [kW]".format(
+                self.total_contribution/1000))
+            print()
         
         return self.total_contribution
             
 
+def place_row_of_heliostats(radius, margin=10.0):
+    """
+    Places a single circle of heliostats, evenly distributed around the circle
+    :param: radius [float] Radius to plot heliostats [m]
+    :param: margin [float] Space between heliostats [m]
+    """
+    global net_power_thermal
+    global solar_field
+
+    r = radius
+    d_s = Heliostat.width + margin
+
+    # message to inform
+    print("  NET POWER THERMAL: {:.1f}".format(net_power_thermal))
+    print("  Placing radius:    {:.1f}".format(r))
+
+    # make d_theta so it is equal all the way around
+    d_theta = arcsin(d_s/r)
+    number = np.floor((2*pi) / d_theta)
+    d_theta = (2*pi) / number 
+
+    # generate list of coordinates, and instantiate heliostats
+    theta = 0.0
+    coordinate_list = []
+    heliostat_row = []
+    while theta < 2*pi:
+        x_coord = r * cos(theta)
+        y_coord = r * sin(theta)
+        z_coord = 0.0
+        coordinate = np.array((x_coord, y_coord, z_coord))
+        coordinate_list.append(coordinate)
+
+        h = Heliostat(coordinate)
+        heliostat_row.append(h)
+        solar_field.append(h)
+        theta += d_theta
+        net_power_thermal += h.total_contribution
+
+    # plot row
+    for h in heliostat_row:
+        ppx, ppy, ppz = zip(list(h.position))
+        plt.scatter(ppx, ppy, color='c', marker='s', s=5)
+        plt.show(block=False)
+            
+    return heliostat_row
+
+def place_layers_of_heliostats(r_min=INNER_RADIUS, r_max=None, row_margin=20.0):
+    """
+    Places successive rows of heliostats, with a defined row_margin of spacing
+    between each row
+    :param: r_min      [float] Inner heliostat radius from tower (to start) [m]
+    :param: r_max      [float] Outer heliostat radius from tower (to stop) [m]
+    :param: row_margin [float] Space between rows [m]
+    """
+    global net_power_thermal
+
+    r = r_min
+    heliostat_rows_list = []
+    if r_max is not None:
+        while r < r_max:
+            heliostat_row = place_row_of_heliostats(radius=r)
+            heliostat_rows_list.append(heliostat_row)
+            r += row_margin
+
+    else:
+        while net_power_thermal < NET_POWER_TH_W:
+            heliostat_row = place_row_of_heliostats(radius=r)
+            heliostat_rows_list.append(heliostat_row)
+            r += row_margin
+
+    return heliostat_rows_list
+
+
 #-------------------------------------------------------------------------------
-# place heliostats
+# generate list of heliostats (coordinates)
 """
-N spirals going out from radius r0.  Each individual spiral arm has spacing S
-between its own revolutions, and spacing s between its arm and the nearest other
-sprial arm.
+Concentric circles of heliostats
 """
-r0 = INNER_RADIUS
-N  = 4
-s  = Heliostat.width
-S  = s*N
+# heliostat_row = place_row_of_heliostats(radius=INNER_RADIUS)
+print("Placing all heliostats...")
+place_layers_of_heliostats(r_max=500)
 
-h1 = Heliostat(np.array((100,-100,0)))
-h1.calculate_energy_contribution()
+print("--------> Done!")
+print("          Placed {} heliostats".format(len(solar_field)))
+# plot heliostat position
+# for h in solar_field:
+#     ppx, ppy, ppz = zip(list(h.position))
+#     plt.scatter(ppx, ppy, color='c', marker='s', s=5)
 
-number_of_heliostats = np.ceil(NET_POWER_W / h1.total_contribution)
-print("Number of heliostats: {:,.0f}".format(number_of_heliostats))
+# h1 = Heliostat(np.array((1000,-1000,0)))
+# h1.calculate_energy_contribution()
+
+# number_of_heliostats = np.ceil(NET_POWER_W / h1.total_contribution)
+# print("Number of heliostats: {:,.0f}".format(number_of_heliostats))
+
+# # plot heliostat position
+# ppx, ppy, ppz = zip(list(h1.position))
+# plt.scatter(ppx, ppy, color='c', marker='s', s=5)
+
+# show plot
+plt.show(block=False)
+
+# (wait for press enter to close plot)
+input()
 
 #-------------------------------------------------------------------------------
 # set heliostat mirror angles (point to receiver)
