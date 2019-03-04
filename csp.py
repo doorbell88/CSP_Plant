5270e31f2078ef9e2ed4a3dd71893aeb7ee9323e
Kevin Klinkel <kgklinkel@gmail.com>
2019-03-04 11:41:12 +0100

    tidying up

--- csp.py
+++ csp.py
@@ -105,7 +105,7 @@ POWER
 NET_POWER_MW            = 0     #[MW]
 
 STORAGE_HOURS           = 8       #[hrs]
-STORAGE_POWER_MW        = 200 #[MW]
+STORAGE_POWER_MW        = 200     #[MW]
 
 EFF_RECEIVER_THERMAL    = 0.88
 EFF_PB                  = 0.43
